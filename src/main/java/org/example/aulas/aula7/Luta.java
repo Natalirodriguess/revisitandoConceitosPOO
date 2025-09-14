e9be5f21ff5768b7ed63124b1ee90a7efed2ca21
Natalirodriguess <natalyrodrigues43210@gmail.com>
2025-09-14 17:41:56 -0300

    [main] criando classe Luta.java

---
+++ src/main/java/org/example/aulas/aula7/Luta.java
@@ -0,0 +1,16 @@
+package org.example.aulas.aula7;
+//um lutador disputa uma luta
+public class Luta {
+    private String desafiado; //aqui será criada a relação de agregação
+    private String desafiante;
+    private Integer rounds;
+    private Boolean aprovada;
+
+    public void marcarLuta(){
+
+    }
+
+    public void lutar(){
+
+    }
+}
