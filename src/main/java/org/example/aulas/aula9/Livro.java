520b8962bf510cb93700d5a43f6d3918970c35ef
natali-rodrigues <natali.silva@rpe.tech>
2025-09-15 23:40:12 -0300

    [001] Add aula 9

---
+++ src/main/java/org/example/aulas/aula9/Livro.java
@@ -0,0 +1,12 @@
+package org.example.aulas.aula9;
+
+public class Livro {
+    String titulo;
+    String autor;
+    Integer totalPaginas;
+    Boolean aberto;
+    Pessoa leitor;
+
+
+
+}
---
+++ src/main/java/org/example/aulas/aula9/Pessoa.java
@@ -0,0 +1,35 @@
+package org.example.aulas.aula9;
+
+public class Pessoa {
+    private String nome;
+    private Integer idade;
+    private String sexo;
+
+    public void fazerAniversario(){
+        this.setIdade(this.getIdade() + 1);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
